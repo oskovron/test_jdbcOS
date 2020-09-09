2c49d2e2fc3c6073cd933380893ad11657031974
Oksana <oscvron@gmail.com>
2020-09-09 15:20:27 +0300

    2

---
+++ src/main/java/model/RegistrationSuccessResponseDTO.java
@@ -0,0 +1,13 @@
+package model;
+
+//package core;
+//
+//import org.testng.annotations.Test;
+//
+//@Test
+public class RegistrationSuccessResponseDTO {
+    // Note: The name should be exactly as the node name is
+    // present in the Json
+    public int id;
+    public String token;
+}
--- src/test/java/tests/RestAssuredPostTestOS.java
+++ src/test/java/tests/RestAssuredPostTestOS.java
@@ -1,5 +1,6 @@
 package tests;
 
+import model.RegistrationSuccessResponseDTO;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.response.ResponseBody;
@@ -17,6 +18,9 @@ public class RestAssuredPostTestOS extends BaseTest{
         RestAssured.baseURI ="https://reqres.in/api";
         RequestSpecification request = RestAssured.given();
 
+
+        // JSONObject is a class that represents a Simple JSON.
+        // We can add Key - Value pairs using the put method
         JSONObject requestParams = new JSONObject();
         requestParams.put("email",  "eve.holt@reqres.in");
         requestParams.put("password", "password1");
@@ -89,4 +93,60 @@ public class RestAssuredPostTestOS extends BaseTest{
     }
 
     // Send a GET request to an Endpoint that expects POST
+    @Test
+    public void getInPOST(){
+        RestAssured.baseURI ="https://reqres.in/api";
+        RequestSpecification request = RestAssured.given();
+
+        JSONObject requestParams = new JSONObject();
+        requestParams.put("email",  "eve.holt@reqres.in");
+        requestParams.put("password", "password1");
+
+        // Add a header stating the Request body is a JSON
+//        request.header("Content-Type", "application/json");
+
+        // Add the Json to the body of the request
+        request.body(requestParams.toJSONString());
+
+        // Post the request and check the response
+        Response response = request.get("/register"); //get
+
+        int statusCode = response.getStatusCode();
+        System.out.println("Status code: " + statusCode);
+        System.out.println("Responce body: " + response.body().toString());
+    }
+
+    //Deserialize json to Class with rest Assured
+    @Test
+    public void deserializeJsonSuccessRegister() {
+        // Tutorial here:
+        // https://www.toolsqa.com/rest-assured/post-request-using-rest-assured/
+        RestAssured.baseURI = "https://reqres.in/api";
+        RequestSpecification request = RestAssured.given();
+
+        // JSONObject is a class that represents a Simple JSON.
+        // We can add Key - Value pairs using the put method
+        JSONObject requestParams = new JSONObject();
+        requestParams.put("email", "eve.holt@reqres.in");
+        requestParams.put("password", "password1");
+
+        // Add a header stating the Request body is a JSON
+        request.header("Content-Type", "application/json");
+
+        // Add the Json to the body of the request
+        request.body(requestParams.toJSONString());
+
+        // Post the request and check the response
+        Response response = request.post("/register");
+        ResponseBody body = response.getBody();
+
+        // Deserialize the Response body into RegistrationSuccessResponse
+        RegistrationSuccessResponseDTO responseBody = body.as(RegistrationSuccessResponseDTO.class);
+
+        // Use the RegistrationSuccessResponse class instance to Assert the values of
+        // Response.
+        Assert.assertEquals(4, responseBody.id);
+        Assert.assertEquals("QpwL5tke4Pnpja7X4", responseBody.token);
+    }
+
 }
